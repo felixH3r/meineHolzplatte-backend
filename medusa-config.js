61a1ae861ed118c00c82a871b30fc623f1f70ad9
FelixH3r <felix.hermanutz@gmx.at>
2024-01-21 07:53:31 +0100

    comment out stripe

--- medusa-config.js
+++ medusa-config.js
@@ -61,13 +61,13 @@ const plugins = [
       prefix: process.env.S3_PREFIX,
     }
   },
-  {
-    resolve: `medusa-payment-stripe`,
-    options: {
-      api_key: process.env.STRIPE_API_KEY,
-      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-    },
-  }
+  // {
+  //   resolve: `medusa-payment-stripe`,
+  //   options: {
+  //     api_key: process.env.STRIPE_API_KEY,
+  //     webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+  //   },
+  // }
 
 
 
